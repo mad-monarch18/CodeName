43739673fd1376107ca5902911f9f1c2cc56dc7d
David Mullen <ty@Davids-MacBook-Pro.local>
2023-10-04 10:21:26 -0500

    warm up

---
+++ Oct4Exercise/src/main/java/com/Oct4Exercise/Oct4Exercise.java
@@ -0,0 +1,12 @@
+package com.Oct4Exercise;
+
+public class Oct4Exercise {
+    public static void main(String[] args) {
+        String personName = ("John Doe");
+        int personAge = 25;
+        double productName = 19.99;
+        float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = true;
+
+    }
+}
